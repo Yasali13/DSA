0a98477e2a749171f0391b41687dfe2b579c89e9
Yasali13 <yasaliedirimanna3@gmail.com>
2025-12-16 14:25:47 +0530

    Add files via upload

---
+++ QueueStackPopFriendly.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dsa;
+
+class QueueStackPopFriendly {
+
+    int size = 100;
+    int[] q1 = new int[size];
+    int[] q2 = new int[size];
+    int front1 = 0, rear1 = 0;
+    int front2 = 0, rear2 = 0;
+
+    // PUSH operation (costly)
+    void push(int x) {
+        // Move all from q1 to q2
+        while (front1 < rear1) {
+            q2[rear2++] = q1[front1++];
+        }
+
+        // Insert new element
+        q1[rear1++] = x;
+
+        // Move back elements
+        while (front2 < rear2) {
+            q1[rear1++] = q2[front2++];
+        }
+
+        front1 = front2 = rear2 = 0;
+        System.out.println(x + " pushed");
+    }
+
+    // POP operation (easy)
+    int pop() {
+        if (front1 == rear1) {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        return q1[front1++];
+    }
+
+    public static void main(String[] args) {
+        QueueStackPopFriendly s = new QueueStackPopFriendly();
+        s.push(15);
+        s.push(20);
+        s.push(25);
+
+        System.out.println("Popped: " + s.pop());
+    }
+}
---
+++ StackQueueEnqueueFriendly.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dsa;
+
+class StackQueueEnqueueFriendly {
+
+    int size = 100;
+    int[] s1 = new int[size];
+    int[] s2 = new int[size];
+    int top1 = -1;
+    int top2 = -1;
+
+    // ENQUEUE operation (easy)
+    void enqueue(int x) {
+        s1[++top1] = x;
+        System.out.println(x + " enqueued");
+    }
+
+    // DEQUEUE operation (costly)
+    int dequeue() {
+        if (top1 == -1) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+
+        while (top1 != -1) {
+            s2[++top2] = s1[top1--];
+        }
+
+        int dequeued = s2[top2--];
+
+        while (top2 != -1) {
+            s1[++top1] = s2[top2--];
+        }
+
+        return dequeued;
+    }
+
+    public static void main(String[] args) {
+        StackQueueEnqueueFriendly q = new StackQueueEnqueueFriendly();
+        q.enqueue(50);
+        q.enqueue(70);
+        q.enqueue(90);
+
+        System.out.println("Dequeued: " + q.dequeue());
+    }
+}
