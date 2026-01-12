0b835495b8c7ff3c7ae247cf2477f52ad5195070
Yasali13 <yasaliedirimanna3@gmail.com>
2026-01-13 00:14:46 +0530

    Add files via upload

---
+++ OptimizedBubbleSort.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dsa;
+
+/**
+ *
+ * @author Yasali
+ */
+
+public class OptimizedBubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped)
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        bubbleSort(arr);
+
+        for (int x : arr)
+            System.out.print(x + " ");
+    }
+}
