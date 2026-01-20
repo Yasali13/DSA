dba3755a6286d4887ec6923d9364e34a0eb69e07
Yasali13 <yasaliedirimanna3@gmail.com>
2026-01-21 00:08:48 +0530

    Add files via upload

---
+++ OptimizedInsertionSort.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dsa;
+
+/**
+ *
+ * @author Yasali
+ */
+public class OptimizedInsertionSort {
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Stop early when position is found
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {8, 3, 5, 2, 9};
+        insertionSort(arr);
+
+        for (int x : arr)
+            System.out.print(x + " ");
+    }
+}
